2b764538717c37cd36a48b460d710660c0acb292
ahmed <ahmed.zaakan@gmail.com>
2024-07-11 11:49:14 +0300

    end structer

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import Weather from './Weather';
+import Calender from './Calender';
+import './News.css'
 
 const News = () => {
   return (
-    <div>
-      Mohmed
+    <div className='news'>
+      <header className="news-header">News Header</header>
+      <div className="news-content">
+        <div className="navbar">
+            <div className="user">User</div>
+            <nav className="categories">Categoris</nav>
+        </div>
+        <div className="news-section">
+            <div className="headline">Head Line</div>
+            <div className="news-grid">News Grid</div>
+        </div>
+        <div className="my-blogs">
+           My Blogs
+        </div>
+        <div className="weather-calender">
+        <Weather />
+        <Calender />
+        </div>
+        
+      </div>
+      <footer className="news-footer">Footer</footer>
     </div>
   )
 }
---
+++ src/Components/Weather.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Weather.css';
+
+const Weather = () => {
+  return (
+    <div className='weather'>
+      Wather
+    </div>
+  )
+}
+
+export default Weather
