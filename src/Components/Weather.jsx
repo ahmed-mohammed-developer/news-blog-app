6123304f95ec1013b1913f2fb74e17432fad6723
ahmed <ahmed.zaakan@gmail.com>
2024-07-14 23:17:47 +0300

    Add Weather

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -4,7 +4,21 @@ import './Weather.css';
 const Weather = () => {
   return (
     <div className='weather'>
-      Wather
+      <div className="search">
+        <div className="search-top">
+          <i className="fa-solid fa-location-dot"></i>
+          <div className="location">Abha</div>
+        </div>
+        <div className="search-location">
+          <input type="text"placeholder='Enter Location' />
+          <i className="fa-solid fa-magnifying-glass"></i>
+        </div>
+      </div>
+      <div className="weather-data">
+      <i className='bx bxs-sun'></i>
+      <div className="weather-type">Claer</div>
+      <div className="temp">31</div>
+      </div>
     </div>
   )
 }
