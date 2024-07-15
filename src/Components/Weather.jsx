6076349a1abf392ed4db9ca5c53cb588c186ae65
ahmed <ahmed.zaakan@gmail.com>
2024-07-15 18:43:03 +0300

    add logic Weather

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,24 +1,91 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import './Weather.css';
 
 const Weather = () => {
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  useEffect(() => {
+      const fetchDefaultLocation = async () => {
+        const defaultLocation = "Abhā"
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${defaultLocation}&units=Metric&appid=5dc31f1a51a862365906ddd9165235ee`
+        const response = await axios.get(url)
+        setData(response.data)
+      }
+      fetchDefaultLocation()
+  }, [])
+
+    const search = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=5dc31f1a51a862365906ddd9165235ee`
+
+      try{
+        const response = await axios.get(url)
+        if(response.data.cod !== 200) {
+          setData({notFound: true})
+        } else {
+          setData(response.data)
+          setLocation('')
+        } 
+      } catch (error){
+        if(error.response && error.response.status === 404){
+          setData({notFound: true})
+        } else {
+          console.error("An unexpected erroroccurred", error)
+        }
+      }
+    }
+
+    const handleInputChange = (e) => {
+      setLocation(e.target.value)
+    }
+     //للبحث عن الطقس بالضغط على انتر
+    const handaleKeyDown = (e) => {
+    if(e.key === "Enter") {
+      search()
+    }
+    }
+
+    const getWeatherIcon = (weatherType) => {
+      switch(weatherType) {
+        case 'Clear':
+          return <i className="bx bxs-sun"></i>
+        case 'Clouds':
+          return <i className="bx bxs-cloud"></i>
+        case 'Rain':
+          return <i className="bx bxs-cloud-rain"></i>
+        case 'Thumderstorm':
+          return <i className="bx bxs-cloud-lightning"></i>
+        case 'Snow':
+          return <i className="bx bxs-cloud-snow"></i>
+        case 'Haze':
+        case 'Mist':
+          return <i className="bx bxs-cloud"></i>
+        default: 
+          return <i className="bx bxs-cloud"></i>
+      }
+    }
+
   return (
     <div className='weather'>
       <div className="search">
         <div className="search-top">
           <i className="fa-solid fa-location-dot"></i>
-          <div className="location">Abha</div>
+          <div className="location">{data.name}</div>
         </div>
         <div className="search-location">
-          <input type="text"placeholder='Enter Location' />
-          <i className="fa-solid fa-magnifying-glass"></i>
+          <input type="text"placeholder='Enter Location' value={location} onChange={handleInputChange}
+          onKeyDown={handaleKeyDown} />
+          <i className="fa-solid fa-magnifying-glass" onClick={search}></i>
         </div>
       </div>
-      <div className="weather-data">
-      <i className='bx bxs-sun'></i>
-      <div className="weather-type">Claer</div>
-      <div className="temp">31</div>
+      {data.notFound ? (<div className='not-found'>Not Found😯</div>) : (
+        <div className="weather-data">
+        {data.weather && data.weather[0] && getWeatherIcon(data.weather[0].main)}
+      <div className="weather-type">{data.weather ? data.weather[0].main : null}</div>
+      <div className="temp">{data.main ? `${Math.floor(data.main.temp)}°` : null}</div>
       </div>
+      )}
     </div>
   )
 }
